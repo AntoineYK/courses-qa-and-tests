2fb4b0b4c651547646507446ae4a607487113b56
AntoineYK <antoinemrl18@gmail.com>
2025-09-25 17:19:11 +0200

    8 - 10

--- tests/ecommerce.spec.ts
+++ tests/ecommerce.spec.ts
@@ -3,5 +3,10 @@ import test from "@playwright/test";
 test.describe("Ecommerce's product page", () => {
   test("should go to product page", async ({ page }) => {
     await page.goto("https://automationexercise.com/");
+
+    // Ceci est un Locator, il permet de localiser un élément de la page
+    // https://playwright.dev/docs/api/class-framelocator#frame-locator-get-by-label
+    // Ici nous localisons le lien vers la page products.
+    const nav = page.getByRole("navigation");
   });
 });
